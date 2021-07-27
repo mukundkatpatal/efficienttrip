e720b1928595bde0495c25f709b604b5e241dbe0
chiragw <chiragwadhwa369@gmail.com>
2021-07-27 17:42:18 +0530

    tournamentForm

---
+++ src/app/app-routing/routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+
+import { AboutComponent } from '../about/about.component';
+
+export const routes: Routes = [
+  { path: 'tournamentForm',  component: AboutComponent }
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -14,23 +13,49 @@ import {
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
+import { AboutComponent } from './about/about.component';
+import { MatSliderModule } from '@angular/material/slider';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { HomeComponent } from './home/home.component';
+
  
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatIconModule,
+    MatSelectModule,
+    MatSlideToggleModule,
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
     MatMenuModule,
-    SocialLoginModule
+    MatButtonModule,
+    MatSliderModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    SocialLoginModule,
+    AppRoutingModule,
+    FormsModule, 
+    ReactiveFormsModule
   ],
   providers: [
     {
