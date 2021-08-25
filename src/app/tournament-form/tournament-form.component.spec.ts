ec4bcb1323ca56dcd11d35faa64fc5029f23d1d3
priti vyawahare <pritiv.acem@gmail.com>
2021-08-25 12:00:29 +0200

    upgraded to 11

--- src/app/tournament-form/tournament-form.component.spec.ts
+++ src/app/tournament-form/tournament-form.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TournaamentFormComponent } from './tournaament-form.component';
 
@@ -6,7 +6,7 @@ describe('TournaamentFormComponent', () => {
   let component: TournaamentFormComponent;
   let fixture: ComponentFixture<TournaamentFormComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TournaamentFormComponent ]
     })
